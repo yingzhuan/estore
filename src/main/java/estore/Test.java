5046329238c280b1ad5a2c7d15837cb18ece2c85
yingzhuan <923273130@qq.com>
2019-07-16 18:10:43 +0800

    init estore project

---
+++ src/main/java/estore/Test.java
@@ -0,0 +1,16 @@
+package estore;
+
+
+public class Test {
+	
+	public static void main(String[] args){
+		
+		System.out.println("Hello World");
+	}
+	
+	@org.junit.Test
+	public void add(){
+		System.out.println("Hello");
+	}
+
+}
